8c6352cb8c49bf0f8c56fb0f2c5d022feb4cc19e
Shawn Lee <shawn.lee.2017@smu.edu.sg>
2019-03-17 22:17:15 +0800

    Almost there

--- sql/delivery.sql
+++ sql/delivery.sql
@@ -4,22 +4,22 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-CREATE DATABASE IF NOT EXISTS `appuser` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `appuser`;
+CREATE DATABASE IF NOT EXISTS `user` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `user`;
 
 
-DROP TABLE IF EXISTS `appuser`;
-CREATE TABLE IF NOT EXISTS `appuser` (
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
   `userid` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(16) NOT NULL,
   `fullname` char(25) NOT NULL,
   `hp` varchar(8) NOT NULL,
-  `userpw` char(16) NOT NULL,
+  `password` char(16) NOT NULL,
   PRIMARY KEY (`userid`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 
-INSERT INTO `appuser` (`userid`, `username`, `fullname`, `hp`, `userpw`) VALUES
+INSERT INTO `user` (`userid`, `username`, `fullname`, `hp`, `password`) VALUES
 (NULL, 'shawnlee95', 'Shawn Lee Min Hwee', '98765432', 'apple123'),
 (NULL, 'lionelng96', 'Lionel Ng Ze Ji', '87654321', 'pear123'),
 (NULL, 'gohyuxin', 'Goh Yu Xin', '76543210', 'orange123');
