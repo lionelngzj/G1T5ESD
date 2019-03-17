1f740434ff91650791e6915ec5f12b9d5945e3eb
yuxin <35590245+yuxinn@users.noreply.github.com>
2019-03-17 17:43:27 +0800

    Added insert statements to dump appuser data

--- sql/delivery.sql
+++ sql/delivery.sql
@@ -14,3 +14,8 @@ CREATE TABLE APPuser
  PhoneNo int NOT NULL,
  UserPW varchar(25) NOT NULL,
  constraint userID_pk primary key (UserID));
+ 
+ INSERT INTO `appuser` (`Username`, `Fullname`, `PhoneNo`, `UserPW`) VALUES 
+ ('shawnleemh', 'Shawn Lee Min Hwee', '97332638', 'shawnleemh'),
+ ('gohyuxin', 'Goh Yu Xin', '92192123', 'iamyuxin'),
+ ('lionelnzj', 'Lionel Ng Ze Ji', '94231231', 'lionelng1996');
