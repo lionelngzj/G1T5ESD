7d57c12dcb547d4bda829d56649bb6ed6a67582e
Shawn Lee <shawn.lee.2017@smu.edu.sg>
2019-03-17 22:13:53 +0800

    Changed User database

--- sql/delivery.sql
+++ sql/delivery.sql
@@ -1,21 +1,25 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
-SET time_zone = "+08:00";
+SET time_zone = "+00:00";
 
-DROP DATABASE IF EXISTS APPuser;
-CREATE DATABASE APPuser DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE APPuser;
 
-CREATE TABLE APPuser
-(UserID int NOT NULL AUTO_INCREMENT,
- Username varchar(25) NOT NULL,
- Fullname varchar(25) NOT NULL,
- PhoneNo int NOT NULL,
- UserPW varchar(25) NOT NULL,
- constraint userID_pk primary key (UserID));
- 
- INSERT INTO `appuser` (`Username`, `Fullname`, `PhoneNo`, `UserPW`) VALUES 
- ('shawnleemh', 'Shawn Lee Min Hwee', '97332638', 'shawnleemh'),
- ('gohyuxin', 'Goh Yu Xin', '92192123', 'iamyuxin'),
- ('lionelnzj', 'Lionel Ng Ze Ji', '94231231', 'lionelng1996');
+CREATE DATABASE IF NOT EXISTS `appuser` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `appuser`;
+
+
+DROP TABLE IF EXISTS `appuser`;
+CREATE TABLE IF NOT EXISTS `appuser` (
+  `userid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(16) NOT NULL,
+  `fullname` char(25) NOT NULL,
+  `hp` varchar(8) NOT NULL,
+  `userpw` char(16) NOT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `appuser` (`userid`, `username`, `fullname`, `hp`, `userpw`) VALUES
+(NULL, 'shawnlee95', 'Shawn Lee Min Hwee', '98765432', 'apple123'),
+(NULL, 'lionelng96', 'Lionel Ng Ze Ji', '87654321', 'pear123'),
+(NULL, 'gohyuxin', 'Goh Yu Xin', '76543210', 'orange123');
