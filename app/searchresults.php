9fc032fea3fc163e4401ab81ca03ef6df48449ee
yuxin <35590245+yuxinn@users.noreply.github.com>
2019-03-19 17:27:34 +0800

    edited search form, added searchresults page
    Yet to configure the searchresults page, only passed the postal code through GET

---
+++ app/searchresults.php
@@ -0,0 +1,26 @@
+<html>
+<style>
+    table#table-restaurant {
+    border-collapse: collapse;   
+    }
+    #table-restaurant td:hover {
+        cursor: pointer;
+    }
+</style>
+
+<head>
+    <title>BragDoof</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div id="container-restaurants"></div>
+    </div>
+
+</body>
+
+</html>
