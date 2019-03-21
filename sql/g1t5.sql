a3244a9dd2b7568c65ae69fd17a78ae115720f0f
Shawn Lee <shawn.lee.2017@smu.edu.sg>
2019-03-21 14:06:06 +0800

    new user

--- sql/g1t5.sql
+++ sql/g1t5.sql
@@ -11,17 +11,18 @@ USE `user`;
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `username` varchar(16) NOT NULL,
-  `fullname` char(25) NOT NULL,
-  `hp` int(8) NOT NULL,
   `password` varchar(16) NOT NULL,
+  `fullname` char(64) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `hpnumber` int(11) NOT NULL,
   `driverwallet` float NOT NULL DEFAULT 0.0,
   PRIMARY KEY (`username`)
 )ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-INSERT INTO `user` (`username`, `fullname`, `hp`, `password`, `driverwallet`) VALUES
-('shawnlee95', 'Shawn Lee Min Hwee', '98765432', 'apple123', NULL),
-('lionelng96', 'Lionel Ng Ze Ji', '87654321', 'pear123', NULL),
-('gohyuxin', 'Goh Yu Xin', '76543210', 'orange123', NULL);
+INSERT INTO `user` (`username`, `password`, `fullname`, `email`, `hpnumber`, `driverwallet`) VALUES
+('shawnlee95', 'shawnlee123', 'Shawn Lee Min Hwee', 'shawn.lee.2017@smu.edu.sg', 98765432, NULL),
+('lionelng96', 'lionelng123', 'Lionel Ng Ze Ji', 'lionel.ng.2017@smu.edu.sg', 87654321, NULL),
+('gohyuxin91', 'gohyuxin123', 'Goh Yu Xin', 'yuxin.goh.2017@smu.edu.sg', 76543210, NULL);
 
 DROP TABLE IF EXISTS `address`;
 CREATE TABLE IF NOT EXISTS `address` (
@@ -30,16 +31,15 @@ CREATE TABLE IF NOT EXISTS `address` (
   `postalcode` int(6) NOT NULL,
   PRIMARY KEY (`username`, `address`),
   CONSTRAINT address_fk FOREIGN KEY (username)
-    REFERENCES consumer (username)
+    REFERENCES user (username)
 )ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 INSERT INTO `address` (`username`, `address`, `postalcode`) VALUES
 ('shawnlee95', '2E Hong San Walk #12-05', '689051'),
 ('lionelng96', 'Blk 512 Wellington Circle #02-18', '750512'),
-('gohyuxin', '11 Chai Chee Road', '460011'),
-('shawnlee95', '2E Hong San Walk #11-05', '689051');
-('lionelng96', '368 Thomson Road', '298127'),
-
+('gohyuxin91', '11 Chai Chee Road', '460011'),
+('shawnlee95', '2E Hong San Walk #11-05', '689051'),
+('lionelng96', '368 Thomson Road', '298127');
 
 -- -----------------------------------------------------------------------------------------
 DROP DATABASE IF EXISTS `foodorders`;
