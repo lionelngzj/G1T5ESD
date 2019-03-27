981f93dbf4e4dbfbe3835e3cc315cc3e798ea541
Shawn Lee <shawn.lee.2017@smu.edu.sg>
2019-03-27 20:44:56 +0800

    FINAL

--- sql/order.sql
+++ sql/order.sql
@@ -4,16 +4,15 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 -- -----------------------------------------------------------------------------------------
-DROP DATABASE IF EXISTS `foodorders`;
-CREATE DATABASE `foodorders` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `foodorders`;
+DROP DATABASE IF EXISTS `order`;
+CREATE DATABASE `order` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `order`;
 
-DROP TABLE IF EXISTS `orderitems`;
 DROP TABLE IF EXISTS `orders`;
 CREATE TABLE IF NOT EXISTS `orders` (
   `orderid` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(16) NOT NULL,
-  `restaurantid` int(11) NOT NULL,
+  `rid` int(11) NOT NULL,
   `date` datetime NOT NULL DEFAULT NOW(),
   `status` varchar(16) NOT NULL,
   `paymentreceipt` varchar(68) NOT NULL,
@@ -21,6 +20,14 @@ CREATE TABLE IF NOT EXISTS `orders` (
   PRIMARY KEY (`orderid`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
+INSERT INTO `orders` (`username`, `rid`, `date`, `status`,`paymentreceipt`, `total_amount`) VALUES
+('shawnlee95', 1, '2019-03-16 12:55:00', 'ready', '#1777-6432', 109.00),
+('yuxin', 2, '2019-03-17 15:40:00', 'ready', '#1777-6433', 13.00),
+('lionelng96', 1, '2019-03-17 17:22:10', 'ready', '#1777-6434', 23.4),
+('yuxin', 1, '2019-03-18 11:32:03', 'paid', '#1777-6435', 18.3);
+
+
+DROP TABLE IF EXISTS `orderitems`;
 CREATE TABLE IF NOT EXISTS `orderitems` (
   `orderid` int(11) NOT NULL,
   `itemid` int(11) NOT NULL,
@@ -30,13 +37,7 @@ CREATE TABLE IF NOT EXISTS `orderitems` (
         REFERENCES orders (orderid)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-INSERT INTO `orders` (`username`, `restaurantid`, `date`, `status`,`paymentreceipt`, `total_amount`) VALUES
-('shawnlee95', 1, '2019-03-16 12:55:00', 'paid', '#1777-6432', 109.00),
-('yuxin', 2, '2019-03-17 15:40:00', 'paid', '#1777-6433', 13.00),
-('lionelng96', 1, '2019-03-17 17:22:10', 'paid', '#1777-6434', 23.4),
-('yuxin', 1, '2019-03-18 11:32:03', 'paid', '#1777-6435', 18.3);
-
-INSERT INTO `orderitems` (`orderid`, `itemid`, `quantity`) VALUES
+INSERT INTO `orderitems` (`orderid`, `fid`, `quantity`) VALUES
 (1, 1, 10),
 (2, 1, 1),
 (2, 2, 1),
