fe66fdedb39396b3fd2e4737f5da52fad63b4b73
Shawn Lee <shawn.lee.2017@smu.edu.sg>
2019-03-27 20:23:05 +0800

    user final

--- sql/user.sql
+++ sql/user.sql
@@ -7,30 +7,15 @@ CREATE TABLE IF NOT EXISTS `user` (
   `username` varchar(16) NOT NULL,
   `password` varchar(16) NOT NULL,
   `fullname` char(64) NOT NULL,
+  `telegramid` char(64) NOT NULL,
   `email` varchar(64) NOT NULL,
   `hpnumber` int(11) NOT NULL,
   PRIMARY KEY (`username`)
 )ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-INSERT INTO `user` (`username`, `password`, `fullname`, `email`, `hpnumber`) VALUES
-('shawnlee95', 'shawnlee123', 'Shawn Lee Min Hwee', 'shawn.lee.2017@smu.edu.sg', 98765432),
-('lionelng96', 'lionelng123', 'Lionel Ng Ze Ji', 'lionel.ng.2017@smu.edu.sg', 87654321),
-('yuxin', '123', 'Goh Yu Xin', 'yuxin.goh.2017@smu.edu.sg', 76543210);
-
-DROP TABLE IF EXISTS `address`;
-CREATE TABLE IF NOT EXISTS `address` (
-  `username` varchar(16) NOT NULL,
-  `street` varchar(64) NOT NULL,
-  `unit` varchar(64),
-  `postalcode` int(6) NOT NULL,
-  PRIMARY KEY (`username`, `street`,`unit`),
-  CONSTRAINT address_fk FOREIGN KEY (username)
-    REFERENCES user (username)
-)ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-INSERT INTO `address` (`username`, `street`, `unit`, `postalcode`) VALUES
-('shawnlee95', '2E Hong San Walk', '#12-05', '689051'),
-('lionelng96', 'Blk 512 Wellington Circle', '#02-18', '750512'),
-('yuxin', '11 Chai Chee Road', NULL,'460011'),
-('shawnlee95', '2E Hong San Walk', '#11-05', '689051'),
-('lionelng96', '368 Thomson Road', NULL, '298127');
+INSERT INTO `user` (`username`, `password`, `fullname`, `telegramid`, `email`, `hpnumber`) VALUES
+('shawnlee95', 'shawnlee123', 'Shawn Lee Min Hwee', 248395638, 'shawn.lee.2017@smu.edu.sg', 98765432),
+('lionelng96', 'lionelng123', 'Lionel Ng Ze Ji', 80771196, 'lionel.ng.2017@smu.edu.sg', 87654321),
+('yuxin', '123', 'Goh Yu Xin', 44771340, 'yuxin.goh.2017@smu.edu.sg', 76543210),
+( "darrenlim96", "darren123", "Darren Lim Cong Hao", 188176270, "darren.lim.2017@smu.edu.sg", 65432109),
+("ignatiustan96", "ignatius", "Ignatius Tan Jun Hong", 157884892,"ignatiustan.2017@sis.smu.edu.sg", 54321098);
