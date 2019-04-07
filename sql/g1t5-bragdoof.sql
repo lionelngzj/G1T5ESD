8ea7ef8ccdab7111815999d295f2e68f8fdf6a57
yuxin <35590245+yuxinn@users.noreply.github.com>
2019-04-08 03:46:35 +0800

    standardized single quote for insert to user

--- sql/g1t5-bragdoof.sql
+++ sql/g1t5-bragdoof.sql
@@ -250,6 +250,6 @@ CREATE TABLE IF NOT EXISTS `user` (
 INSERT INTO `user` (`username`, `password`, `fullname`, `telegramid`, `email`, `hpnumber`) VALUES
 ('shawnlee95', 'shawnlee123', 'Shawn Lee Min Hwee', 248395638, 'shawn.lee.2017@smu.edu.sg', 98765432),
 ('lionelng96', 'lionelng123', 'Lionel Ng Ze Ji', 80771196, 'lionel.ng.2017@smu.edu.sg', 87654321),
-('yuxin', '123', 'Goh Yu Xin', 44771340, 'yuxin.goh.2017@smu.edu.sg', 76543210),
-( "darrenlim96", "darren123", "Darren Lim Cong Hao", 188176270, "darren.lim.2017@smu.edu.sg", 65432109),
-("ignatiustan96", "ignatius", "Ignatius Tan Jun Hong", 157884892,"ignatiustan.2017@sis.smu.edu.sg", 54321098);
+('yuxin', '123', 'Goh Yu Xin', 44771340, 'yuxin.goh.2017@smu.edu.sg', 96543210),
+( 'darrenlim96', 'darren123', 'Darren Lim Cong Hao', 188176270, 'darren.lim.2017@smu.edu.sg', 65432109),
+('ignatiustan96', 'ignatius', 'Ignatius Tan Jun Hong', 157884892,'ignatiustan.2017@sis.smu.edu.sg', 94321098);
