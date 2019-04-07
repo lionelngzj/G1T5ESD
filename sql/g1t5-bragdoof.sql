b5da419ff5f9beeaef4530ccf932540afc0f99ba
yuxin <35590245+yuxinn@users.noreply.github.com>
2019-04-08 02:42:21 +0800

    set time zone for sql

--- sql/g1t5-bragdoof.sql
+++ sql/g1t5-bragdoof.sql
@@ -1,7 +1,7 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone = "+08:00";
 
 -- -----------------------------------------------------------------------------------------
 DROP DATABASE IF EXISTS `order`;
