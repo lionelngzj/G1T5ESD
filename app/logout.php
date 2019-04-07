0bb7fc15771e6df53ed7ca1893e139c70aefd934
yuxin <35590245+yuxinn@users.noreply.github.com>
2019-04-08 02:50:17 +0800

    removed files that are not needed

--- app/logout.php
+++ app/logout.php
@@ -2,5 +2,7 @@
   session_start();
   unset($_SESSION["usertype"]);
   unset($_SESSION["username"]);
+  unset($_SESSION["rid"]);
+  unset($_SESSION["price"]);
   header("Location: index.php", true, 301);
 ?>
